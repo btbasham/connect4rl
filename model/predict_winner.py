cb3c987b0299149b61a9ddbfb901d8603768e7f1
Brian Basham <btbasham@enlitic.com>
2016-05-28 13:50:32 -0700

    accounting for draws, transforming board to two channels in training script

--- model/predict_winner.py
+++ model/predict_winner.py
@@ -10,8 +10,8 @@ import numpy as np
 
 
 # For now don't roll back, just take final board
-NUM_EXAMPLES = 10000
-NUM_TRAIN_EXAMPLES = 8000
+NUM_EXAMPLES = 100000
+NUM_TRAIN_EXAMPLES = 80000
 NUM_TEST_EXAMPLES = NUM_EXAMPLES - NUM_TRAIN_EXAMPLES
 games = []
 winners = []
@@ -28,11 +28,11 @@ for i in range(NUM_EXAMPLES):
     #num_steps_back = np.random.randint(7)
     # for i in range(num_steps_back):
     #    my_game.unmake_move()
-    games.append(my_game.board)
-    winners.append(winner)
+    if winner != 2:
+        games.append(my_game.board)
+        winners.append(winner)
 
     # TODO: represent games as 3 channels instead of 1
-
 
 print "calculated winners"
 games = np.array(games)
@@ -42,14 +42,23 @@ batch_size = 200
 num_rows = connect4.NUM_ROWS
 num_columns = connect4.NUM_COLUMNS
 
+
+def transform_two_channels(game_board):
+    assert len(game_board.shape) == 2
+    return np.stack((game_board == 0, game_board == 1))
+
+print games.shape
+games = np.array([transform_two_channels(game) for game in games])
+print games.shape
+
 X_train = games[:NUM_TRAIN_EXAMPLES]
 Y_train = winners[:NUM_TRAIN_EXAMPLES]
 
 X_test = games[NUM_TRAIN_EXAMPLES:]
 Y_test = winners[NUM_TRAIN_EXAMPLES:]
 
-X_train = X_train.reshape(-1, 1, num_rows, num_columns)
-X_test = X_test.reshape(-1, 1, num_rows, num_columns)
+X_train = X_train.reshape(-1, 2, num_rows, num_columns)
+X_test = X_test.reshape(-1, 2, num_rows, num_columns)
 X_train.astype('float32')
 X_test.astype('float32')
 Y_train = Y_train.reshape(-1, 1).astype('float32')
@@ -64,7 +73,7 @@ model = Sequential()
 
 model.add(Convolution2D(50, 2, 2,
                         border_mode='valid',
-                        input_shape=(1, num_rows, num_columns)))
+                        input_shape=(2, num_rows, num_columns)))
 model.add(Activation('relu'))
 model.add(Convolution2D(50, 2, 2, border_mode='valid',))
 model.add(Activation('relu'))
@@ -86,7 +95,7 @@ model.compile(
     loss='binary_crossentropy', optimizer='adadelta', class_mode='binary')
 
 print "compiled model"
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=1000,
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=50,
           show_accuracy=True, verbose=1, validation_data=(X_test, Y_test))
 print "fit"
 score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
